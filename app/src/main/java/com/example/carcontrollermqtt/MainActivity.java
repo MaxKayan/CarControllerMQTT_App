50b81964151a335d011fc68e21c88197b6cd0836
MaxKayan <maxim.kayander1@gmail.com>
2020-11-02 00:12:45 +0300

    Added app bar configuration and onSupportNavigateUp

--- app/src/main/java/com/example/carcontrollermqtt/MainActivity.java
+++ app/src/main/java/com/example/carcontrollermqtt/MainActivity.java
@@ -7,7 +7,10 @@ import android.view.Window;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.carcontrollermqtt.databinding.ActivityMainBinding;
 
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
     MainViewModel viewModel;
     private ActivityMainBinding binding;
+
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +32,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-//                R.id.navigation_devices, R.id.navigation_dashboard, R.id.navigation_history, R.id.navigation_settings)
-//                .build();
+        appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_devices, R.id.navigation_dashboard, R.id.navigation_history, R.id.navigation_settings)
+                .build();
 //        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 //        NavigationUI.setupWithNavController(binding.navView, navController);
@@ -63,5 +68,12 @@ public class MainActivity extends AppCompatActivity {
         super.onAttachedToWindow();
         Window window = getWindow();
         window.setFormat(PixelFormat.RGBA_8888);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
